a2765575d6a3ffd88cf852c71c2078748f577138
SpencerFlorence@gmail.com <SpencerFlorence@gmail.com@ef7e1c36-7004-04e1-fb1f-4d9bb1d6646f>
2011-07-27 16:52:14 +0000

    switched vectors to lists added insertion tests

--- trunk/db-source.rkt
+++ trunk/db-source.rkt
@@ -12,7 +12,7 @@ with keys representing name of the table
 
 A Table is a:
 (table [listof Symbol]
-       [vectorof Record]))
+       [listof Record]))
 with the symbols being field names
 field names in Fields MUST match the field names. 
 
@@ -21,11 +21,11 @@ a Record is a:
 keys are names of each field
 
 a Query is a:
-(query [vectorof Record])
+(query [listof Record])
 |#
 
 (struct database (name raw) #:transparent)
-(struct table (fields raw) #:transparent)
+(struct table (fields [raw #:mutable]) #:transparent)
 (struct record (raw) #:transparent)
 (struct query (raw) #:transparent)
 
@@ -73,14 +73,12 @@ a Query is a:
     [(_ db (join (from1 from2) in w:whns ...))
      #'(join db from1 from1 in (list (λ (r) (w.check (hash-ref (record-raw r) w.field))) ...))]))
 
-;; select
-
 ;; Database [Listof fieldnames] tablename [Listof [Record -> Any]] -> Query
 ;; queries the DB for selected info
 (define (select db whats from whens)
   (define tbl (hash-ref from (database-raw db)))
   (query
-   (for/vector ([rcrd (table-raw tbl)]
+   (for/list ([rcrd (table-raw tbl)]
                 #:when (for/and ([f whens])
                          (f rcrd)))
      (record 
@@ -94,15 +92,15 @@ a Query is a:
   (define v1 (table-raw (hash-ref (database-raw db) from1)))
   (define v2 (table-raw (hash-ref (database-raw db) from2)))
   
-  (query (for*/vector ([r1 v1]
+  (query (for*/list ([r1 v1]
                        [r2 v2]
                        #:when (and (equal? (hash-ref (record-raw r1) on) (hash-ref (record-raw r2) on))
                                    (for/and ([f whens])
-                                     (f r1)
-                                     (f r2))))
-           (let ([r2.1 (hash-copy (record-raw r2))])
-             (hash-remove! r2.1 on)
-           (record (hash!-append (record-raw r1) r2.1))))))
+                                     (and (f r1)
+                                          (f r2)))))
+           (let ([r2/1 (hash-copy (record-raw r2))])
+             (hash-remove! r2/1 on)
+           (record (hash!-append (record-raw r1) r2/1))))))
                       
 ;; hash hash -> hash
 ;; appends two hashes
@@ -113,24 +111,36 @@ a Query is a:
   
 ;; effect (insert,delete, create)
 ;; ---------------------------------------------------------------------------------------------------
+
 (define-syntax (effect stx)
   (syntax-parse stx
-    #:literals (#;insert #;delete create drop #;update)
+    #:literals (insert #;delete create drop #;update)
     [(_ db (create name (fields ...)))
      #'(create db name (list fields ...))]
     [(_ db (drop table))
-     #'(drop db table)]))
+     #'(drop db table)]
+    [(_ db (insert (values ...) table-name))
+     #'(insert db table-name (list values ...))]))
 
 ;; update
 
-;; insert
+;; Database TableName [Listof values] -> (void)
+;; inserts a record in to the given table with the given values
+(define (insert db tbl values)
+  (let* ([tbl (hash-ref (database-raw db) tbl)]
+         [l (table-raw tbl)])
+    (set-table-raw! tbl
+                     (cons
+                      (record (make-hash (for/list ([fname (table-fields tbl)]
+                                                    [v values])
+                                           (cons fname v)))) l))))
 
 ;; delete
 
 ;; Database TableName [listof symbols] -> (void)
 ;; creates a new table in db with the corresponding tables
 (define (create db tbl-name fields)
-  (hash-set! (database-raw db) tbl-name (table fields (vector))))
+  (hash-set! (database-raw db) tbl-name (table fields (list))))
 
 ;; Database TableName -> (void)
 ;; drops the given table
@@ -203,8 +213,8 @@ a Query is a:
 (define (serialize-table tbl)
   (to-string 
    `(table "'",(table-fields tbl)
-           (vector ,@(for/list ([rec (table-raw tbl)])
-                       (serialize-record rec))))))
+           "(list",@(for/list ([rec (table-raw tbl)])
+                       (serialize-record rec))")")))
 
 ;; record -> string
 ;; converts the record to a string that when used as the arg to (eval (read (string->port str))) would result in the same raw record
@@ -239,24 +249,40 @@ a Query is a:
 ;                                          
 ;                                          
 ;                                          
-;                  
+;          
+(define-test-suite --querying
+  )
 (define-test-suite --effecting
   ;; create
   (let ([db (make-empty-database "")])
     (create db 'test '(oh hai there))
-    (check-equal? db (database "" (make-hash (list (cons 'test (table '(oh hai there) (vector))))))))
+    (check-equal? db (database "" (make-hash (list (cons 'test (table '(oh hai there) (list))))))))
   (let ([db (make-empty-database "")])
     (effect db (create 'test ('oh 'hai 'there)))
-    (check-equal? db (database "" (make-hash (list (cons 'test (table '(oh hai there) (vector))))))))
+    (check-equal? db (database "" (make-hash (list (cons 'test (table '(oh hai there) (list))))))))
   ;; drop
   (let ([db (make-empty-database "")])
     (create db 'test '(oh hai there))
+    (check-equal? db (database "" (make-hash (list (cons 'test (table '(oh hai there) (list)))))))
     (drop db 'test)
     (check-equal? db (make-empty-database "")))
   (let ([db (make-empty-database "")])
     (create db 'test '(oh hai there))
+    (check-equal? db (database "" (make-hash (list (cons 'test (table '(oh hai there) (list)))))))
     (effect db (drop 'test))
     (check-equal? db (make-empty-database "")))
+  ;; insert 
+  (let ([db (make-empty-database "")])
+    (create db 'test '(oh hai there))
+    (check-equal? db (database "" (make-hash (list (cons 'test (table '(oh hai there) (list)))))))
+    (insert db 'test '(1 2 3))
+    (check-equal? db (database "" (make-hash (list (cons 'test (table '(oh hai there) (list (record (make-hash (list (cons 'oh 1) (cons 'hai 2) (cons 'there 3))))))))))))
+  (let ([db (make-empty-database "")])
+    (create db 'test '(oh hai there))
+    (check-equal? db (database "" (make-hash (list (cons 'test (table '(oh hai there) (list)))))))
+    (effect db (insert (1 2 3) 'test))
+    (check-equal? db (database "" (make-hash (list (cons 'test (table '(oh hai there) (list (record (make-hash (list (cons 'oh 1) (cons 'hai 2) (cons 'there 3))))))))))))
+  
   )
 (define-test-suite --loading
   ;; string->value
@@ -280,36 +306,36 @@ a Query is a:
                                               (list 
                                                (cons "test"
                                                      (table '(name id)
-                                                            (vector))))))))
+                                                            (list))))))))
                 (make-hash 
                  (list 
                   (cons "test"
                         (table '(name id)
-                               (vector))))))
+                               (list))))))
   (check-equal? (string->value (serialize (database
                                              "" 
                                              (make-hash 
                                               (list 
                                                (cons "test"
                                                      (table '(name id)
-                                                            (vector (record (make-hash (list (cons 'name "spencer")
+                                                            (list (record (make-hash (list (cons 'name "spencer")
                                                                                              (cons 'id "000532210"))))))))))))
                 (make-hash 
                  (list 
                   (cons "test"
                         (table '(name id)
-                               (vector (record (make-hash (list (cons 'name "spencer")
+                               (list (record (make-hash (list (cons 'name "spencer")
                                                                 (cons 'id "000532210"))))))))))
   ;; serialize-table
   (check-equal? (string->value (serialize-table (table '(name id)
-                                                         (vector))))
+                                                         (list))))
                 (table '(name id)
-                       (vector)))
+                       (list)))
   (check-equal? (string->value (serialize-table (table '(name id)
-                                                         (vector (record (make-hash (list (cons 'name "spencer")
+                                                         (list (record (make-hash (list (cons 'name "spencer")
                                                                                           (cons 'id "000532210"))))))))
                 (table '(name id)
-                       (vector (record (make-hash (list (cons 'name "spencer")
+                       (list (record (make-hash (list (cons 'name "spencer")
                                                         (cons 'id "000532210")))))))
   ;; serialize-record
   (check-equal? (string->value (serialize-record (record (make-hash (list (cons 'name "spencer")
@@ -327,6 +353,7 @@ a Query is a:
                 'yes?)
   )
 
+(run-tests --querying)
 (run-tests --effecting)
 (run-tests --loading)
 (run-tests --serialize)
