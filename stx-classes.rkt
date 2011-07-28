acaae9bfce685bd44e2a057c4864136edb22867c
SpencerFlorence@gmail.com <SpencerFlorence@gmail.com@ef7e1c36-7004-04e1-fb1f-4d9bb1d6646f>
2011-07-28 17:09:00 +0000

    trying to get querying macro working. it does with a where clause.... fuckers....

--- stx-classes.rkt
+++ stx-classes.rkt
@@ -1,5 +1,27 @@
 #lang racket
 (require syntax/parse)
-(provide (all-defined-out))
+(require "db-data.rkt")
+(provide whn)
+(define-namespace-anchor current)
+
+(define-syntax-class whn
+  #:attributes (func)
+  (pattern (~or w:whns #;w:and-whns #;w:or-whns)
+           #:attr func
+           #'w.func))
 (define-syntax-class whns
-  (pattern (field check)))
+  (pattern (field check)
+           #:attr func
+           #'(λ (r) (check (hash-ref (record-raw r) field)))))
+
+#;(define-syntax-class and-whns
+  #:literals (and)
+  (pattern (and w:whn ...)
+           #:attr func
+           (λ (r) (and (w.func r) ...))))
+
+#;(define-syntax-class or-whns
+  #:literals (or)
+  (pattern (or w:whn ...)
+           #:attr func
+           (λ (r) (or ((attribute w.func) r) ...))))
